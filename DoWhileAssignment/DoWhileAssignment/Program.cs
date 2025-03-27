7b1ef30ab4e5c4911e4c768a5e88274c54b5690f
ncory44 <ncory44@gmail.com>
2025-03-27 09:46:50 -0700

    do while assignment

---
+++ DoWhileAssignment/DoWhileAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Boolean comparison with a While Loop");
+
+        int count = 0;
+
+        while (count < 10)
+        {
+            Console.WriteLine("Count is "+ count);
+            count++;
+        }
+
+        Console.WriteLine("\nBoolean comparison with a Do While Loop");
+
+        int number;
+        do
+        {
+            Console.Write("Enter a number greater than 10: ");
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+        while (number <= 10);
+
+        Console.WriteLine("You entered: " + number);
+    }
+}
