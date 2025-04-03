e1ca0dc1f976a6c397f27c1c929c89dfaed646cf
ncory44 <ncory44@gmail.com>
2025-04-02 21:59:18 -0700

    calling methods assignment

---
+++ CallingMethodsAssignment/MathOperations.cs
@@ -0,0 +1,20 @@
+public class MathOperations
+{
+    //method adding 10
+    public int AddTen(int number)
+    {
+        return number + 10;
+    }
+
+    //method multiplying by 5
+    public int MultiplyByFive(int number)
+    {
+        return number * 5;
+    }
+
+    //method subtracting 3
+    public int SubtractThree(int number)
+    {
+        return number - 3;
+    }
+}
---
+++ CallingMethodsAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ask the user for a number
+        Console.WriteLine("Please enter a number:");
+        string input = Console.ReadLine();
+        int usersNumber = Convert.ToInt32(input);
+
+        //creates an instance of the MathOperations class
+        MathOperations math = new MathOperations();
+
+        //calls the AddTen method and stores the result
+        int added = math.AddTen(usersNumber);
+        Console.WriteLine("Your number plus 10 is: " + added);
+
+        //calls the MultiplyByFive method and stores the result
+        int multiplied = math.MultiplyByFive(usersNumber);
+        Console.WriteLine("Your number multiplied by 5 is: " + multiplied);
+
+        //calls the SubtractThree method and stores the result
+        int subtracted = math.SubtractThree(usersNumber);
+        Console.WriteLine("Your number minus 3 is: " + subtracted);
+    }
+}
