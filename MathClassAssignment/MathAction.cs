118feefadbbe3f2debe8c48be46a0d56ec051f97
ncory44 <ncory44@gmail.com>
2025-04-03 23:50:23 -0700

    math class assignment

---
+++ MathClassAssignment/MathAction.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class MathAction
+{
+    public void DoSomething(int x, int y)
+    {
+        int result = x * 2;
+
+        Console.WriteLine("The first number doubled is: " + result);
+
+        Console.WriteLine("The second number is: " + y);
+    }
+}
---
+++ MathClassAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //instantiate the MathAction class
+        MathAction mathAction = new MathAction();
+
+        //call the DoSomething method
+        Console.WriteLine("Calling method passing in two numbers:");
+        mathAction.DoSomething(5, 10);
+
+        //call the doSomething method with specified parameters
+        Console.WriteLine("\nCalling method passing in two numbers with specified parameters:");
+        mathAction.DoSomething(x: 7, y: 14);
+    }
+}
