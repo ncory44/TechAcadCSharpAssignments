ccde4d841e1ca9a3caedf7527f17fc490eae53f8
ncory44 <ncory44@gmail.com>
2025-04-21 22:45:35 -0700

    date and time assignment

---
+++ DateAndTimeAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //prints current date and time
+        DateTime now = DateTime.Now;
+        Console.WriteLine("Current Date and Time: " + now);
+
+        //asks the user for number of hours
+        Console.Write("Enter the number of hours to add: ");
+        string input = Console.ReadLine();
+
+        //parses the input to an integer
+        if (int.TryParse(input, out int hoursToAdd))
+        {
+            //adds the hours to the current date and time
+            DateTime futureTime = now.AddHours(hoursToAdd);
+            Console.WriteLine("In {0} hour(s), the time will be {1}", hoursToAdd, futureTime );
+        }
+        else
+        {
+            Console.WriteLine("Please enter a valid number.");
+        }
+
+        Console.ReadLine();
+    }
+}
